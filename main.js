ee6c8a7345a0ddb0969669b98a934a17c91a365c
Nick Gottlieb <ngottlieb@gmail.com>
2021-11-07 15:38:09 -0800

    switch to select for choosing map

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
+// initialize info modal and show it on pageload
+const modal = new bootstrap.Modal(document.getElementById('infoModal'));
+modal.show();
+
 mapboxgl.accessToken = 'pk.eyJ1IjoibmdvdHRsaWViIiwiYSI6ImNrdm9vNGh3ODB3ZTkybm1vdHFmajVsNXIifQ.O3HhHIdFv0L_5FreT4GLjA';
 
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/mapbox/streets-v11', // style URL
   center: [-125.701, 54.916], // starting position [lng, lat]
-  zoom: 5 // starting zoom
+  zoom: 7 // starting zoom
 });
 
+let layerDefinitions;
+let activeLayer;
+
 map.on('load', async () => {
-  let layerDefinitions;
   layerDefinitions = await fetch("./layerDefinitions.json").then(response => response.json());
 
   // set up mouseover popup
@@ -21,6 +27,10 @@ map.on('load', async () => {
 
   // add data layers
   layerDefinitions.forEach((layer, index) => {
+    if (index === 0) {
+      activeLayer = layer;
+    }
+
     map.addSource(layer.name, {
       type: 'vector',
       url: `mapbox://${layer.tilesetId}`
@@ -57,19 +67,49 @@ map.on('load', async () => {
       map.getCanvas().style.cursor = '';
       popup.remove();
     });
+
+    // show zoom help text when layer is invisible
+    map.on('zoomend', updateZoomWarning);
   });
   
 
   // configure accordion to show and hide layers when they are toggled
-  const layerAccordion = document.getElementById("accordionMapData");
-  layerAccordion.addEventListener('show.bs.collapse', function (e) {
-    const layer = e.target.dataset.layerName;
-
-    map.setLayoutProperty(layer, 'visibility', 'visible');
-  });
-  layerAccordion.addEventListener('hide.bs.collapse', function (e) {
-    const layer = e.target.dataset.layerName;
-
-    map.setLayoutProperty(layer, 'visibility', 'none');
+  const mapSelector = document.getElementById("mapSelector");
+  mapSelector.addEventListener('change', function () {
+    switchToLayer(this.value);
   });
 });
+
+function switchToLayer(layerName) {
+  activeLayer = layerDefinitions.find(x => x.name === layerName);
+
+  // load the map description
+  const mapDescription = document.getElementById("mapDescription");
+  var newDescription = "";
+  if (activeLayer.shortDescription) {
+    newDescription += `<p><strong>${activeLayer.shortDescription}</strong></p>`;
+  }
+  newDescription += activeLayer.description;
+
+  mapDescription.innerHTML = newDescription;
+
+  // set visibility appropriately
+  layerDefinitions.forEach(l => {
+    map.setLayoutProperty(l.name, 'visibility', activeLayer === l ? 'visible' : 'none');
+  });
+
+  updateZoomWarning();
+}
+
+function updateZoomWarning() {
+  const currZoom = map.getZoom();
+  const activeSource = map.getSource(activeLayer.name);
+  const minZoom = activeSource.minzoom;
+  const zoomHelpText = document.getElementById('zoomHelpText');
+
+  if (currZoom < minZoom) {
+    zoomHelpText.style = "";
+  } else {
+    zoomHelpText.style = "display: none !important;";
+  }
+}
